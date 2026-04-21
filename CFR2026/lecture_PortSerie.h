dc9138738ed983ace44c114eb55094f42505e66f
ARTeC-Robotique <151625142+ARTeC-Robotique@users.noreply.github.com>
2026-04-21 18:19:40 +0200

    Add files via upload

---
+++ CFR2026/lecture_PortSerie.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <Arduino.h>
+
+struct Point {
+  char    com[16]; //ca c'est la commandes
+  int16_t x;
+  int16_t y;
+};
+
+extern Point p;
+extern bool  nouveau_message;  // true quand un message vient d'arriver
+
+void portSerie_init();
+void portSerie_lecture();
